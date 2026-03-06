fb20d8ba1866c21b7ab0873908353f03630d3e32
PaulH4lbr3in <35578320+PaulH4lbr3in@users.noreply.github.com>
2026-03-06 12:32:19 +0100

    Tests eingeführt um Ranking miteinzubeziehen.

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -54,4 +54,54 @@ class LeaderboardControllerTests {
         assertEquals(third, res[1])
     }
 
+    @Test
+    fun test_getLeaderboardWithBadRank() {
+        val player1 = GameResult(1, "player1", 20, 20.0)
+        val player2 = GameResult(2, "player2", 20, 10.0)
+        val player3 = GameResult(3, "player3", 20, 15.0)
+        val player4 = GameResult(1, "player4", 20, 20.0)
+        val player5 = GameResult(2, "player5", 20, 10.0)
+        val player6 = GameResult(3, "player6", 20, 15.0)
+        val player7 = GameResult(3, "player7", 20, 15.0)
+
+        whenever(mockedService.getGameResults()).thenReturn(listOf(player1, player2, player3, player4, player5, player6, player7))
+
+        assertThrows<ResponseStatusException> {
+            controller.getLeaderboard(-1)
+        }
+        assertThrows<ResponseStatusException> {
+            controller.getLeaderboard(10)
+        }
+    }
+
+    @Test
+    fun test_getLeaderboardWithByRank() {
+        val player1 = GameResult(1, "player1", 20, 10.0)
+        val player2 = GameResult(2, "player2", 20, 15.0)
+        val player3 = GameResult(3, "player3", 20, 20.0)
+        val player4 = GameResult(4, "player4", 15, 10.0)
+        val player5 = GameResult(5, "player5", 15, 15.0)
+        val player6 = GameResult(6, "player6", 15, 20.0)
+        val player7 = GameResult(7, "player7", 10, 15.0)
+
+        whenever(mockedService.getGameResults()).thenReturn(listOf(player1, player2, player3, player4, player5, player6, player7))
+
+        val res: List<GameResult> = controller.getLeaderboard(6)
+        assertEquals(4, res.size)
+        assertEquals("player4", res[0].playerName)
+        assertEquals("player7", res[3].playerName)
+    }
+
+    @Test
+    fun test_getLeaderboardWithShortList() {
+        val player1 = GameResult(1, "player1", 20, 20.0)
+        val player2 = GameResult(2, "player2", 20, 10.0)
+        val player3 = GameResult(3, "player3", 20, 15.0)
+
+        whenever(mockedService.getGameResults()).thenReturn(listOf(player1, player2, player3))
+
+        val res: List<GameResult> = controller.getLeaderboard(0)
+        assertEquals(3, res.size)
+        assertEquals("player2", res[0].playerName)
+    }
 }
