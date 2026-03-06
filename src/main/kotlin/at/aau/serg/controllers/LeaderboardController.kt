4544055f47fd529d824edea08795c0b892ff7549
PaulH4lbr3in <35578320+PaulH4lbr3in@users.noreply.github.com>
2026-03-06 12:23:16 +0100

    Suche nach Rang implementiert

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -16,4 +16,18 @@ class LeaderboardController(
     fun getLeaderboard(): List<GameResult> =
         gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { +it.timeInSeconds}))
 
+
+    @GetMapping
+    fun getLeaderboard(rank: Int): List<GameResult> { //Rank is index of list (Starting with 0)
+        var leaderboard = getLeaderboard()
+
+        if (rank < 0 || rank >= leaderboard.size) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Rank out of bounds")
+        }
+
+        val startIndex = maxOf(0, rank - 3)
+        val endIndex = minOf(leaderboard.size - 1, rank + 3)
+
+        return leaderboard.slice(startIndex..endIndex)
+    }
 }
